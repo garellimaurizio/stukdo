7f4eb8a81008f4c9f796888430458415d2157739
Maurizio Garelli <garellimaurizio@outlook.com>
2015-05-30 16:06:18 +0200

    Added state to our tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,10 @@ class TasksController < ApplicationController
 
   def index
 #    @tasks = Task.all #this line fetch all the Tasks instead I need only all the task of a specific user
-    @tasks = current_user.tasks
+#    @tasks = current_user.tasks
+	@to_do = current_user.tasks.where(state: "to_do")
+	@doing = current_user.tasks.where(state: "doing")
+	@done = current_user.tasks.where(state: "done")
     respond_with(@tasks)
   end
 
@@ -45,6 +48,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :state)
     end
 end
